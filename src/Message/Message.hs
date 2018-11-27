556fcd447f6da6b3f843504f7d63459f3ad9f2c7
Sasa Bogicevic <t4nt0r@protonmail.com>
2018-11-27 17:06:38 +0100

    Add async and message severity

--- src/Message/Message.hs
+++ src/Message/Message.hs
@@ -1,36 +1,31 @@
-{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE InstanceSigs        #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Message.Message where
 
+import Control.Monad (forever, void)
+import Control.Concurrent.Async
+import Control.Concurrent (forkIO)
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TBChan
 import Control.Monad.IO.Class
 import Data.HashMap.Lazy
 import Data.Text (Text)
 
+data Severity = Info | Warn | Error deriving (Eq, Show)
+
 data Message =
   Message
   { body :: Text
+  , severity :: Severity
   } deriving (Eq, Show)
 
-newtype Mlist a = Mlist { runMlist :: (TBChan a) }
+newtype Mlist a = Mlist { runMlist :: TBChan a }
 
 addMessage :: Mlist Message -> Message -> STM ()
-addMessage ml msg = writeTBChan (runMlist ml) msg
+addMessage ml = writeTBChan (runMlist ml)
 
 readMessage :: Mlist Message -> STM Message
 readMessage ml = readTBChan (runMlist ml)
-
-writeRead :: Message -> IO ()
-writeRead m = do
-  msg <-
-    atomically $ do
-      ml <- createMlist
-      addMessage ml m
-      readMessage ml
-  putStrLn (show msg)
 
 class HasMlist a where
   addMsg :: MonadIO m => Mlist a -> a -> m ()
@@ -45,3 +40,7 @@ createMlist = do
   chan <- newTBChan 1000000
   pure $ Mlist chan
 
+runner :: Mlist Message -> IO ()
+runner ml = void $ async $ forever $ do
+  msg <- atomically $ readMessage ml -- this is where we send message out
+  print msg
